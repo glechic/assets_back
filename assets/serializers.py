8e23a71235684797bb85f920cdef07dc04196486
glechic <serg.pash99@gmail.com>
2020-06-02 21:01:37 +0300

    fix choices issue on serializers

--- assets/serializers.py
+++ assets/serializers.py
@@ -5,6 +5,8 @@ from .models import Asset, Request
 
 class AssetSerializer(serializers.ModelSerializer):
     owner = serializers.CharField()
+    type = serializers.CharField(source='get_type_display')
+    status = serializers.CharField(source='get_status_display')
 
     class Meta:
         model = Asset
